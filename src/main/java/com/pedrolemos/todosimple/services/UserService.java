33a2aa9525afd95a6e392ce45f2f156badba9b09
Pedro Henrique Nascimento Lemos <pedroefernando38@gmail.com>
2024-06-18 22:46:49 -0300

    Refactor userService

--- src/main/java/com/pedrolemos/todosimple/services/UserService.java
+++ src/main/java/com/pedrolemos/todosimple/services/UserService.java
@@ -1,7 +1,6 @@
 package com.pedrolemos.todosimple.services;
 
 import com.pedrolemos.todosimple.models.User;
-import com.pedrolemos.todosimple.repositories.TaskRepository;
 import com.pedrolemos.todosimple.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,8 +14,6 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    @Autowired
-    private TaskRepository taskRepository;
 
     public User findById(Long id){
         Optional<User> user = this.userRepository.findById(id);
@@ -29,7 +26,6 @@ public class UserService {
     public User create(User obj) {
         obj.setId(null);
         obj = this.userRepository.save(obj);
-        this.taskRepository.saveAll(obj.getTasks());
         return obj;
     }
 
